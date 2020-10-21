5fa29d661ec20712591120a4ddb532255d595b48
craftwerk <pst@crftwrk.de>
2020-10-21 10:48:14 +0200

    3.4.0

--- woocommerce/loop/pagination.php
+++ woocommerce/loop/pagination.php
@@ -28,24 +28,7 @@ if ( $total <= 1 ) {
 	return;
 }
 ?>
-<nav class="woocommerce-pagination">
-	<?php
-	echo paginate_links(
-		apply_filters(
-			'woocommerce_pagination_args',
-			array( // WPCS: XSS ok.
-				'base'      => $base,
-				'format'    => $format,
-				'add_args'  => false,
-				'current'   => max( 1, $current ),
-				'total'     => $total,
-				'prev_text' => '&larr;',
-				'next_text' => '&rarr;',
-				'type'      => 'list',
-				'end_size'  => 3,
-				'mid_size'  => 3,
-			)
-		)
-	);
-	?>
-</nav>
+    <!-- Pagination -->
+    <div>
+        <?php bootscore_pagination(); ?>
+    </div>
