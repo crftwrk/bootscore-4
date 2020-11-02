186f179abf6e5d8ca36c306afdf3068e2ff9f171
craftwerk <pst@crftwrk.de>
2020-11-02 11:00:02 +0100

    3.4.4

--- readme.txt
+++ readme.txt
@@ -6,7 +6,7 @@ Tags: featured-images, threaded-comments, translation-ready
 Requires at least: 4.5
 Tested up to: 5.5
 Requires PHP: 5.6
-Stable tag: 3.4.3
+Stable tag: 3.4.4
 License: GNU General Public License v2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
@@ -47,6 +47,10 @@ https://bootscore.me/category/documentation/
 == Changelog ==
 
 == Changelog ==
+
+    = 3.4.4 - November 2 2020 =
+
+        * Removed id from searchform.php
 
     = 3.4.3 - October 30 2020 =
 
--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 
 <!-- Search Button Outline Secondary Right -->
 <form class="searchform input-group" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>" class="form-inline">
-    <input type="text" name="s" class="form-control" id="inlineFormInputGroup" placeholder="<?php _e('Search', 'bootscore'); ?>">   
+    <input type="text" name="s" class="form-control" placeholder="<?php _e('Search', 'bootscore'); ?>">   
     <div class="input-group-append">
         <button type="submit" class="btn btn-outline-secondary"><i class="fas fa-search"></i></button>
     </div>
